cce0889e88b8a139f3bafb97f4e62482f5107cbc
misspatriana <leeken851@gmail.com>
2025-02-14 12:19:33 +0800

    activity 1
    Added Java Application

---
+++ IT13A_SANDOY/src/it13a_sandoy/IT13A_SANDOY.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package it13a_sandoy;
+
+/**
+ *
+ * @author Administrator
+ */
+public class IT13A_SANDOY {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
