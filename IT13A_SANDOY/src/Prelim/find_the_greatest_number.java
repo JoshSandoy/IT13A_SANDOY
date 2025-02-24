c9ef884f4c8d64cc7ad96b2ae8d8f02636b14bc1
JoshSandoy <leeken851@gmail.com>
2025-02-24 21:22:12 +0800

    Update find_the_greatest_number.java

--- IT13A_SANDOY/src/Prelim/find_the_greatest_number.java
+++ IT13A_SANDOY/src/Prelim/find_the_greatest_number.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Prelim;
-
-/**
- *
- * @author Administrator
- */
-public class find_the_greatest_number {
+import java.util.Scanner;
+public class Find_the_greatest_number {
     
-}
+    public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    
+        
+    System.out.println("Input the first number");
+    int vegeta = scan.nextInt();   
+    
+    System.out.println("Input the second number");
+    int guko = scan.nextInt();  
+    
+    System.out.println("Input the third number");
+    int guhan = scan.nextInt();  
+       
+    if(vegeta >guko && vegeta>guhan){System.out.println("the greatest number is : "+vegeta);
+    
+    }else if(guko >vegeta && guko>guhan){System.out.println("the greatest number is : "+guko);
+    
+    }else if(guhan>guko && guhan>vegeta){ System.out.println("the greatest number is : "+guhan);
+    
+    }else{System.out.println("kjasbdksadk");}
+    }
+    }
+
